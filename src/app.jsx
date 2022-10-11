a2ac0da97e9d48374860cef260c6718022fc022a
yooveloper <yooveloper@gmail.com>
2022-10-11 15:17:54 +0900

    ✨ feat: 드림코딩 연습 문제 추가

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import './app.css';
+import Habits from './components/habits';
+
+function App() {
+  return (
+    <Habits />
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './app.jsx';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
