ce6b7571e8a2a373798c0d200a66f15075ddcfde
yooveloper <yooveloper@gmail.com>
2022-10-13 21:29:24 +0900

    💄 add: habits ui 추가

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -3,18 +3,30 @@ import Habit from './habit';
 
 class Habits extends Component {
 
-  state = {
-    habits: [
-      { id: 1, name: 'Reading', count: 0},
-      { id: 2, name: 'Running', count: 0},
-      { id: 3, name: 'Coding', count: 0},
-    ],
-  };
+  handleIncrement = habit => {
+    this.props.onIncrement(habit);
+  }
+
+  handleDecrement = habit => {
+    this.props.onDecrement(habit);
+  }
+
+  handleDelete = habit => {
+    this.props.onDelete(habit);
+  }
 
   render() {
     return (
       <ul>
-        {this.state.habits.map(habit => (<Habit key={habit.id} habit={habit}/>))}
+        {this.props.habits.map(habit => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+          ))}
       </ul>
     );
   }
