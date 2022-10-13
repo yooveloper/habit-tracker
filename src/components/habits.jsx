bd4c36c1d1bca5f1af21a2b9acee892b6a61c28f
yooveloper <yooveloper@gmail.com>
2022-10-13 22:42:06 +0900

    💄 update: app, habits 컴포넌트 성능 최적화

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Habit from './habit';
+import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
 
@@ -15,19 +16,27 @@ class Habits extends Component {
     this.props.onDelete(habit);
   }
 
+  handleAdd = name => {
+    this.props.onAdd(name);
+  }
+
   render() {
     return (
-      <ul>
-        {this.props.habits.map(habit => (
-          <Habit
-            key={habit.id}
-            habit={habit}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}
-          />
-          ))}
-      </ul>
+      <>
+        <HabitAddForm onAdd={this.handleAdd}/>
+        <ul>
+          {this.props.habits.map(habit => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.handleIncrement}
+              onDecrement={this.handleDecrement}
+              onDelete={this.handleDelete}
+            />
+            ))}
+        </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>Reset All</button>
+      </>
     );
   }
 }
