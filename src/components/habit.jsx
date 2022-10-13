87503ecd0bbc7fe09ca4280d760ff933d97b082a
yooveloper <yooveloper@gmail.com>
2022-10-13 21:29:08 +0900

    💄 add: habit ui 추가

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -2,34 +2,32 @@ import React, { Component } from 'react';
 
 class Habit extends Component {
 
-  state = {
-    count: 0,
-  };
-
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit);
   }
 
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit);
   }
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  }
+
 
   render() {
     const { name, count } = this.props.habit;
-    console.log('name : ', name);
-    console.log('count : ', count);
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
         <button className="habit-button habit-increase" onClick={this.handleIncrement}>
           <i className="fas fa-plus-square"></i>
         </button>
         <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
           <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button className="habit-button habit-delete" onClick={this.handleDelete}>
           <i className="fas fa-trash"></i>
         </button>
       </li>
