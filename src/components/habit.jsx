87eec2a96f679b89420e737d26812f818b9a5ee8
yooveloper <yooveloper@gmail.com>
2022-10-17 20:51:28 +0900

    ✅ add: lifecyle 써보기

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,6 +1,13 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Habit extends Component {
+class Habit extends PureComponent {
+
+  componentDidMount() {
+    console.log(`habit: ${this.props.habit.name} mounted`);
+  }
+  componentWillUnmount() {
+    console.log(`habit: ${this.props.habit.name} will mounted`);
+  }
 
   handleIncrement = () => {
     this.props.onIncrement(this.props.habit);
